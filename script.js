eb29819125d02a4f2ba0ce0168e103c2401cd947
PedrohsLeite01 <pedrohslbr@gmail.com>
2024-12-09 15:35:58 -0300

    aiai
    ahahaha

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const alreadyAnimated = localStorage.getItem('aos-animated');
-  
-    if (!alreadyAnimated) {
-      // Inicializa o AOS
-      AOS.init({
-        duration: 800,
-        easing: 'ease-in-out',
-        once: true
-      });
-  
-      // Marca como "já animado"
-      localStorage.setItem('aos-animated', 'true');
-    } else {
-      // Remove as animações (ou mantém o estado final)
-      const elements = document.querySelectorAll('[data-aos]');
-      elements.forEach(el => {
-        el.removeAttribute('data-aos'); // Remove o atributo para evitar animação
-        el.style.opacity = 1; // Garante que os elementos fiquem visíveis
-      });
-    }
+
+ AOS.init({
+    duration: 1000,
+    easing: 'ease-out-back',
+    once: true
 });
